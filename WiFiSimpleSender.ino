cfc246224773384872fe8905ca996765203e38e2
Madhav <103959778+therock297@users.noreply.github.com>
2022-09-30 18:15:10 +0530

    Add files via upload

---
+++ WiFiSimpleSender.ino
@@ -0,0 +1,147 @@
+
+// Name: Madhav
+
+const int TriggerPin = 12;
+const int EchoPin = 11;
+long Duration = 0;
+
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_ARCH_ESP32)
+  #include <WiFi.h>
+#endif
+
+#include "arduino_secrets.h"
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate 
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "broker.hivemq.com";
+int        port     = 1883;
+const char topic[]  = "SIT210/_wave";
+
+const long interval = 1000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID, if not set the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  pinMode(TriggerPin,OUTPUT);
+  pinMode(EchoPin,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(TriggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TriggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+
+  Duration = pulseIn(EchoPin,HIGH);
+
+  long Distance_cm = Distance(Duration);
+
+  Serial.print("Distance = ");
+  Serial.print(Distance_cm);
+  Serial.println(" cm");
+
+  delay(1000);
+  // call poll() regularly to allow the library to send MQTT keep alives which
+  // avoids being disconnected by the broker
+  mqttClient.poll();
+
+  // to avoid having delays in loop, we'll use the strategy from BlinkWithoutDelay
+  // see: File -> Examples -> 02.Digital -> BlinkWithoutDelay for more info
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time a message was sent
+    previousMillis = currentMillis;
+
+    
+
+
+    if( Distance_cm <=50 && Distance_cm != 0)
+    {
+
+      
+      Serial.print("Sending message to topic: ");
+      Serial.println(topic);
+      Serial.print("Wave: ");
+      Serial.println(count);
+
+    // send message, the Print interface can be used to set the message contents
+      mqttClient.beginMessage(topic);
+      mqttClient.print("Wave: ");
+      mqttClient.print(count);
+      mqttClient.print(", Wave at Distance= ");
+      mqttClient.print(Distance_cm);
+      mqttClient.endMessage();
+      Serial.println(); 
+      count++; 
+    }
+    
+  }
+}
+
+long Distance(long time)
+{
+  long DistanceCalc;
+  
+  DistanceCalc = ((time * 0.034) / 2);
+  return DistanceCalc;
+}
